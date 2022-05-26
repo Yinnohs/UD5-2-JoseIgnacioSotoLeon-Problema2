2450dd45f3cf036018963f9bf4424193deddec80
josesoto <josesoto@itop.es>
2022-05-26 10:23:26 +0100

    feat: create vehicle interface

---
+++ src/utils/vehicle/Vehicle.java
@@ -0,0 +1,8 @@
+package utils.vehicle;
+
+public interface Vehicle {
+        public float energyLevel();
+        public void recharge();
+        public void move();
+        public String status();
+}
